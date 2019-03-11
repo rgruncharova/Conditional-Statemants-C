17dec490a4af041600fd92b983fbfde8184180cd
rgruncharova <47794613+rgruncharova@users.noreply.github.com>
2019-03-12 00:30:00 +0200

    Add files via upload

---
+++ AreaFigures.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FigureType = Console.ReadLine();
+            double area = 0.0;
+            switch (FigureType)
+            {
+                case "square":
+                    {
+                        double a = Double.Parse(Console.ReadLine());
+                        area = a * a;
+                        Console.WriteLine($"{area:f3}");
+                        break;
+                    }
+                case "rectangle":
+                    {
+                        double a = Double.Parse(Console.ReadLine());
+                        double b = Double.Parse(Console.ReadLine());
+                        area = a * b;
+                        Console.WriteLine($"{area:f3}");
+                        break;
+                    }
+                case "circle":
+                    {
+                        double r = Double.Parse(Console.ReadLine());
+                        area = (r * r * Math.PI);
+                        Console.WriteLine($"{area:f3}");
+                        break;
+                    }
+                case "triangle":
+                    {
+                        double h = Double.Parse(Console.ReadLine());
+                        double b = Double.Parse(Console.ReadLine());
+                        area = (h * b) / 2;
+                        Console.WriteLine($"{area:f3}");
+                        break;
+                    }
+
+                   
+            }
+        }
+    }
+}
---
+++ Solarship.cs
@@ -0,0 +1,53 @@
+﻿using System;
+class Program
+{
+    static void Main() // 100/100
+    {
+        double leva = double.Parse(Console.ReadLine());
+        double averageGrade = double.Parse(Console.ReadLine());
+        double minumSalary = double.Parse(Console.ReadLine());
+
+        if (averageGrade <= 4.50)
+        {
+            Console.WriteLine("You cannot get a scholarship!");
+        }
+        else if (averageGrade > 4.50 && averageGrade < 5.50)
+        {
+            if (leva > minumSalary)
+            {
+                Console.WriteLine("You cannot get a scholarship!");
+            }
+            else
+            {
+                Console.WriteLine("You get a Social scholarship {0} BGN", Math.Floor(0.35 * minumSalary));
+            }
+        }
+        else if (averageGrade >= 5.50)
+        {
+            if (leva < minumSalary)
+            {
+                double exselentScolarship1 = 0;
+                double exselentScolarship2 = 0;
+
+                exselentScolarship1 = Math.Floor(averageGrade * 25);
+                exselentScolarship2 = Math.Floor(0.35 * minumSalary);
+
+                double scolarship = Math.Max(exselentScolarship1, exselentScolarship2);
+
+                if (exselentScolarship1 == scolarship)
+                {
+                    Console.WriteLine("You get a scholarship for excellent results {0} BGN", scolarship);
+                }
+                else if (exselentScolarship2 == scolarship)
+                {
+                    Console.WriteLine("You get a Social scholarship {0} BGN", scolarship);
+                }
+            }
+            else
+            {
+                double scolarship = Math.Floor(averageGrade * 25);
+                Console.WriteLine("You get a scholarship for excellent results {0} BGN", scolarship);
+            }
+        }
+    }
+}
